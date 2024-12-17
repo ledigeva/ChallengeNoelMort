e594cc6324effbd2e69340b86e967871ab375003
ledigeva <eva.ledig67@gmail.com>
2024-12-17 16:31:27 +0100

    okkk

---
+++ Interface/salleattente.cpp
@@ -0,0 +1,38 @@
+#include "salleattente.h"
+#include "ui_salleattente.h"
+
+SalleAttente::SalleAttente(QWidget *parent)
+    : QWidget(parent)
+    , ui(new Ui::SalleAttente)
+{
+    ui->setupUi(this);
+    ui->label->setStyleSheet("background: transparent;"
+                             "color: white;");
+    ui->labelSalleAttente->setStyleSheet("background: transparent;"
+                                         "color: white;");
+    ui->label_2->setStyleSheet("background: transparent;"
+                               "color: white;");
+    ui->label_3->setStyleSheet("background: transparent;"
+                               "color: white;");
+    ui->label_4->setStyleSheet("background: transparent;"
+                               "color: white;");
+    ui->listWidgetJoueur->setStyleSheet("background: transparent;"
+                                        "border: 1px solid white;"
+                                        "color: white;");
+    ui->listWidgetJoueurPret->setStyleSheet("background: transparent;"
+                                            "border: 1px solid white;"
+                                            "color: white;");
+
+}
+
+SalleAttente::~SalleAttente()
+{
+    delete ui;
+}
+
+void SalleAttente::on_pushButtonPret_clicked()
+{
+    jeuxactif = new JeuxActif;
+    jeuxactif->show();
+}
+
---
+++ Interface/salleattente.h
@@ -0,0 +1,28 @@
+#ifndef SALLEATTENTE_H
+#define SALLEATTENTE_H
+
+#include <QWidget>
+#include "jeuxactif.h"
+#include "jeux.h"
+
+namespace Ui {
+class SalleAttente;
+}
+
+class SalleAttente : public QWidget
+{
+    Q_OBJECT
+
+public:
+    explicit SalleAttente(QWidget *parent = nullptr);
+    ~SalleAttente();
+
+private slots:
+    void on_pushButtonPret_clicked();
+
+private:
+    Ui::SalleAttente *ui;
+    JeuxActif *jeuxactif;
+};
+
+#endif // SALLEATTENTE_H
